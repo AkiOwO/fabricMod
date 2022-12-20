d467e96c320c7ca81174707e407c522d5e002aac
Aki <53384165+AkiOwO@users.noreply.github.com>
2022-12-20 22:44:03 +0100

    Initial Commit #2

---
+++ src/main/java/net/aki/fabricmod/FabricMod.java
@@ -0,0 +1,17 @@
+package net.aki.fabricmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FabricMod implements ModInitializer {
+
+	public static final String MOD_ID = "fabricMod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	
+	@Override
+	public void onInitialize() {
+
+	}
+}
